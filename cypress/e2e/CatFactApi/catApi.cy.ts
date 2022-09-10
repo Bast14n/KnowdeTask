96b858a5fe90924c0bcdf12971db5a8242c887ea
Sebastian Surowka <sebastian.surowka@gmail.com>
2022-09-10 12:27:10 +0200

    refactor api tests

--- cypress/e2e/CatFactApi/catApi.cy.ts
+++ cypress/e2e/CatFactApi/catApi.cy.ts
@@ -1,9 +1,16 @@
 describe('Perform api tests', () => {
     it('#1 When user get random fact about cats #Then proper response should be returned', () => {
         cy.request('https://catfact.ninja/fact').then(response => {
-            assert(response.status === 200, `Expected: ${response.status} to equal: 200`)
-            assert((typeof response.body.fact === 'string') && response.body.fact != '', `Fact field equal: ${response.body.fact}`)
-            assert((typeof response.body.length === 'number') && response.body.length > 0, `Length field equal: ${response.body.length}`)
+            const responseStatus = response.status;
+            const catFact = response.body.fact;
+            const factLength = response.body.length;
+
+            assert(responseStatus === 200, `Expected: ${responseStatus} to equal: 200`)
+            assert((typeof catFact === 'string') && (catFact != ''), `Checking if: ${catFact} - is string and is not empty`);
+            assert((typeof factLength === 'number') && (factLength > 0), `Checking if: ${factLength} - is number and is not empty`);
+
+            cy.log('cat fact: ' + catFact);
+            cy.log('fact length: ' + factLength);
         })
     })
 })
